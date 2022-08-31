b70366867cfd74e70615d3ab380d2ba02f52c53f
Connor Mitchener <lightsoutconnor1@gmail.com>
2022-08-31 13:33:26 -0700

    routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,11 @@
+
+const router = require('express').Router();
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser, createFriend, deleteFriend } = require('../../controllers/user-controller');
+
+router.route('/').get(getAllUsers).post(createUser);
+
+router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(createFriend).delete(deleteFriend);
+
+module.exports = router;
